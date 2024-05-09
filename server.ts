69c9e0be252665880dd362fd40144cfc5d6c28da
Tsa2341 <alanshema2002@gmail.com>
2024-05-09 02:38:49 +0200

    connected fronted url to cors

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ const app = Express();
 app.use(morgan('dev'));
 app.use(
   Cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', `${process.env.FRONTEND_URL}`],
     credentials: true,
   })
 );
