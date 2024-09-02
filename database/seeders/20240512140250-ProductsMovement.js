48053d784411b0a9ecc3e6f071d90fba5b3e2ab3
MPUHWEZIMANA Jean Gabriel <88791661+Gabriel-mdv@users.noreply.github.com>
2024-09-02 11:35:32 +0200

    clear the database leaving some tables (#22)

--- database/seeders/20240512140250-ProductsMovement.js
+++ database/seeders/20240512140250-ProductsMovement.js
@@ -5,46 +5,46 @@
 module.exports = {
 
   async up(queryInterface) {
-    await queryInterface.bulkInsert(
-      'ProductsMovements',
-      [
-        {
-          // This should always be there. It is the main store main
-          id: 'a90c1a62-5e1d-4bf7-b902-8d74c89644d1',
-          quantity: 12,
-          to: '143e4667-a81d-12d3-c356-469311174300',
-          from: '143e4667-a81d-12d3-c356-469311174300',
-          productId: 'b3c15f17-2756-434d-a01e-0b1e7209cb47',
-          userId: '8215a8ea-cf39-4037-81e6-86f6b439dcf4',
-          createdAt: new Date().toISOString(),
-        },
+    // await queryInterface.bulkInsert(
+    //   'ProductsMovements',
+    //   [
+    //     // {
+    //     //   // This should always be there. It is the main store main
+    //     //   id: 'a90c1a62-5e1d-4bf7-b902-8d74c89644d1',
+    //     //   quantity: 12,
+    //     //   to: '143e4667-a81d-12d3-c356-469311174300',
+    //     //   from: '143e4667-a81d-12d3-c356-469311174300',
+    //     //   productId: 'b3c15f17-2756-434d-a01e-0b1e7209cb47',
+    //     //   userId: '8215a8ea-cf39-4037-81e6-86f6b439dcf4',
+    //     //   createdAt: new Date().toISOString(),
+    //     // },
 
 
-        // Store 2
-        {
-          id: 'd2fe9b71-6fa2-49c8-b556-5f7a76ebaa8f',
-          quantity: 12,
-          from: '143e4667-a81d-12d3-c356-469311174300',
-          to: '143e4667-a81d-12d3-c356-469311174301',
-          productId: 'b3c15f17-2756-434d-a01e-0b1e7209cb47',
-          userId: '8215a8ea-cf39-4037-81e6-86f6b439dcf4',
-          createdAt: new Date().toISOString(),
-        },
+    //     // // Store 2
+    //     // {
+    //     //   id: 'd2fe9b71-6fa2-49c8-b556-5f7a76ebaa8f',
+    //     //   quantity: 12,
+    //     //   from: '143e4667-a81d-12d3-c356-469311174300',
+    //     //   to: '143e4667-a81d-12d3-c356-469311174301',
+    //     //   productId: 'b3c15f17-2756-434d-a01e-0b1e7209cb47',
+    //     //   userId: '8215a8ea-cf39-4037-81e6-86f6b439dcf4',
+    //     //   createdAt: new Date().toISOString(),
+    //     // },
 
 
-        // store 3
-        {
-          id: 'f1d8d843-36b7-462e-9b05-409a1db4a9d7',
-          quantity: 12,
-          from: '143e4667-a81d-12d3-c356-469311174300',
-          to: '143e4667-a81d-12d3-c356-469311174302',
-          productId: 'b3c15f17-2756-434d-a01e-0b1e7209cb47',
-          userId: '8215a8ea-cf39-4037-81e6-86f6b439dcf4',
-          createdAt: new Date().toISOString(),
-        },
-      ],
-      {}
-    );
+    //     // // store 3
+    //     // {
+    //     //   id: 'f1d8d843-36b7-462e-9b05-409a1db4a9d7',
+    //     //   quantity: 12,
+    //     //   from: '143e4667-a81d-12d3-c356-469311174300',
+    //     //   to: '143e4667-a81d-12d3-c356-469311174302',
+    //     //   productId: 'b3c15f17-2756-434d-a01e-0b1e7209cb47',
+    //     //   userId: '8215a8ea-cf39-4037-81e6-86f6b439dcf4',
+    //     //   createdAt: new Date().toISOString(),
+    //     // },
+    //   ],
+    //   {}
+    // );
   },
 
   async down(queryInterface) {
--- database/seeders/20240512175148-Transaction.js
+++ database/seeders/20240512175148-Transaction.js
@@ -4,175 +4,175 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.bulkInsert(
-      'Transactions',
-      [
-        {
-          "id": "a90c1a62-5e1d-4bf7-b902-8d74c89644d1",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "b1d6a7f4-5e9c-4bf9-a732-7e67b5a9c1e5",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "c2e7b8c5-6f0d-4bfa-b843-8f78c6a1d2f6",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "34567890-12de-4f01-4567-89012def0123",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "23456789-01cd-4ef0-3456-78901cdef016",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "12345678-90bc-4def-2345-67890bcdef08",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "12345678-90bc-4def-2345-67890bcdef01",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "a1234567-89ab-4cde-f012-3456789abcde",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "b2345678-90bc-4def-0123-4567890bcdef",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "c3456789-01cd-4ef0-1234-5678901cdef0",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "d4567890-12de-4f01-2345-6789012def05",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "e5678901-23ef-4f12-3456-7890123ef012",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "f6789012-34f0-4f23-4567-8901234f0123",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "01234567-89ab-4cde-1234-56789abcdef0",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "12345678-90bc-4def-2345-67890bcdef00",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "23456789-01cd-4ef0-3456-78901cdef012",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        },
-        {
-          "id": "34567890-12de-4f01-4567-89012def0127",
-          "amount": 1200,
-          "description": "Initial deposit",
-          "storeId": "143e4667-a81d-12d3-c356-469311174300",
-          "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
-          "type": "EXPENSE",
-          "createdAt": "2024-06-09T12:34:56.789Z"
-        }
-      ],
+    // await queryInterface.bulkInsert(
+    //   'Transactions',
+    //   [
+    //     // {
+    //     //   "id": "a90c1a62-5e1d-4bf7-b902-8d74c89644d1",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "b1d6a7f4-5e9c-4bf9-a732-7e67b5a9c1e5",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "c2e7b8c5-6f0d-4bfa-b843-8f78c6a1d2f6",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "34567890-12de-4f01-4567-89012def0123",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "23456789-01cd-4ef0-3456-78901cdef016",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "12345678-90bc-4def-2345-67890bcdef08",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "12345678-90bc-4def-2345-67890bcdef01",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "a1234567-89ab-4cde-f012-3456789abcde",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "b2345678-90bc-4def-0123-4567890bcdef",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "c3456789-01cd-4ef0-1234-5678901cdef0",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "d4567890-12de-4f01-2345-6789012def05",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "e5678901-23ef-4f12-3456-7890123ef012",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "f6789012-34f0-4f23-4567-8901234f0123",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "01234567-89ab-4cde-1234-56789abcdef0",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "12345678-90bc-4def-2345-67890bcdef00",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "23456789-01cd-4ef0-3456-78901cdef012",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // },
+    //     // {
+    //     //   "id": "34567890-12de-4f01-4567-89012def0127",
+    //     //   "amount": 1200,
+    //     //   "description": "Initial deposit",
+    //     //   "storeId": "143e4667-a81d-12d3-c356-469311174300",
+    //     //   "userId": "8215a8ea-cf39-4037-81e6-86f6b439dcf4",
+    //     //   "type": "EXPENSE",
+    //     //   "createdAt": "2024-06-09T12:34:56.789Z"
+    //     // }
+    //   ],
 
-      {}
-    );
+    //   {}
+    // );
   },
 
   async down(queryInterface) {
